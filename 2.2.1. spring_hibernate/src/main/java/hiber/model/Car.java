3c9ac929bf9c18381883e37140bbf9a89b8a5759
gavrilsenpai <19oxlopkov99@gmail.com>
2025-04-24 14:01:41 +0300

    1

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,50 @@
+package hiber.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String model;
+    private int series;
+
+    public Car() {}
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    // геттеры и сеттеры
+
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
+
+    public int getSeries() { return series; }
+    public void setSeries(int series) { this.series = series; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+
+    //    @Override
+//    public String toString() {
+//        return model + " " + series;
+//    }
+}
